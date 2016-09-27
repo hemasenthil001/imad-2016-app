f8e4c76bdfab75233d97fcde49781db1bc8dfa35
hemasenthil001 <hemasenthil001@gmail.com>
2016-09-27 16:06:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,15 @@ console.log('Loaded!');
 var text=document.getElementById('main-text');
 text.innerHTML = 'new value';
 var img = document.getElementById('main-img');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+   img.style.marginLeft=marginLeft+ 'px'; 
+}
 
 img.onclick = function() {
-    img.style.marginLeft='150px';
+    var interval=setInterval(moveRight,50);
+    
 };
-console.log('after onclick');
+
+//console.log('after onclick');
