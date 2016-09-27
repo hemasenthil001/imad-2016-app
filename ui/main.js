912fd4f6b78cbad8ac40f53fd925c0ddf2820710
hemasenthil001 <hemasenthil001@gmail.com>
2016-09-27 16:11:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ console.log('Loaded!');
 var text=document.getElementById('main-text');
 text.innerHTML = 'new value';
 var img = document.getElementById('main-img');
+
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+10;
